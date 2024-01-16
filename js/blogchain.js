2e6ee26215217d2b88144b3b20ca6b0a33fd8b23
Lukas Frey <l.frey.inf21@stud.bbbaden.ch>
2024-01-16 15:42:45 +0100

    Added like and save functions

---
+++ js/blogchain.js
@@ -0,0 +1,71 @@
+const postContainer = document.getElementById("postContainer");
+
+fetch("URL", {
+    headers: {
+        "content-type": "application/json",
+        credentials: "same-origin",
+      },
+})
+.then((response) => response.json())
+.then((json) => {
+    json.forEach(element => {
+        let post = document.createElement("div");
+        post.classList.add("post");
+        post.id = element.id;
+        
+        let topline = document.createElement("div");
+        topline.classList.add("topLine");
+        let profileImg = document.createElement("img");
+        profileImg.src = element.profileImage;
+        profileImg.alt = "";
+        let username = document.createElement("span");
+        username.classList.add("username");
+        username.innerHTML = element.username;
+        let time = document.createElement("span");
+        time.classList.add("time");
+        time.innerHTML = element.time;
+
+        topline.appendChild(profileImg);
+        topline.appendChild(username);
+        topline.appendChild(time);
+        post.appendChild(topline);
+
+        let content = document.createElement("div");
+        content.classList.add("content");
+        let caption = document.createElement("div");
+        caption.classList.add("caption");
+        caption.innerHTML = element.caption;
+
+        content.appendChild(caption);
+        post.appendChild(content);
+
+        let bottomline = document.createElement("div");
+        bottomline.classList.add("botomLine");
+        let likebtn = document.createElement("img");
+        likebtn.src = element.likeButtonsrc;
+        likebtn.alt = "";
+        likebtn.id = `like${element.id}`;
+        let commentbtn = document.createElement("img");
+        commentbtn.src = "http://127.0.0.1:5500/img/comment.png";
+        commentbtn.alt = "";
+        let savebtn = document.createElement("img");
+        savebtn.src = element.saveButtonsrc;
+        savebtn.alt = "";
+        savebtn.id = `save${element.id}`;
+
+        bottomline.appendChild(likebtn);
+        bottomline.appendChild(commentbtn);
+        bottomline.appendChild(savebtn);
+        post.appendChild(bottomline);
+
+        postContainer.appendChild(post);
+
+        document.getElementById(`like${element.id}`).onclick = function() {
+            likePost(element.id);
+        };
+
+        document.getElementById(`save${element.id}`).onclick = function() {
+            savePost(element.id);
+        };
+    });
+})
--- js/postInteractions.js
+++ js/postInteractions.js
@@ -1,19 +1,117 @@
-let like = document.getElementById("like");
+let jwt = sessionStorage.getitem("token");
 
-like.addEventListener("click", () => {
+function likePost(id) {
+  let like = document.getElementById(`like${id}`);
+
   if (like.src == "http://127.0.0.1:5500/img/like.png") {
-    like.src = "http://127.0.0.1:5500/img/likedNew.png";
-  } else {
-    like.src = "http://127.0.0.1:5500/img/like.png";
-  }
-});
+      like.src = "http://127.0.0.1:5500/img/likedNew.png";
+  
+    fetch("URL" + id, {
+      headers: {
+        "content-type": "application/json",
+        credentials: "same-origin",
+      },
+    })
+    .then((response) => response.json())
+    .then(data => {
 
-let save = document.getElementById("save");
+      let userWhoLiked = data.userliked;
+      userWhoLiked.push(userId);
 
-save.addEventListener("click", () => {
-  if (save.src == "http://127.0.0.1:5500/img/save.png") {
-    save.src = "http://127.0.0.1:5500/img/saved.png";
-  } else {
-    save.src = "http://127.0.0.1:5500/img/save.png";
-  }
-});
+      fetch("URL" + id, {
+        method: "PUT",
+        headers: {
+            "content-type": "application/json",
+            credentials: "same-origin",
+        },
+        body: JSON.stringify({
+            userliked: userWhoLiked
+        })
+      })
+    })
+    } else {
+      like.src = "http://127.0.0.1:5500/img/like.png";
+
+      fetch("URL" + id, {
+        headers: {
+          "content-type": "application/json",
+          credentials: "same-origin",
+        },
+      })
+      .then((response) => response.json())
+      .then(data => {
+  
+        let userWhoLiked = data.userliked;
+        let userToDislike = userWhoLiked.indexOf(userId);
+
+        if(userToDislike > -1) {
+          userWhoLiked.splice(userToDislike, 1);
+        }
+  
+        fetch("URL" + id, {
+          method: "PUT",
+          headers: {
+              "content-type": "application/json",
+              credentials: "same-origin",
+          },
+          body: JSON.stringify({
+              userliked: userWhoLiked
+          })
+      })
+      })
+  
+    }
+}
+
+function savePost(id) {
+  let save = document.getElementById(`save${id}`);
+
+  fetch("URL" + userId, {
+    headers: {
+      "content-type": "application/json",
+      credentials: "same-origin",
+    },
+  })
+  .then((response) => response.json())
+  .then(data => {
+
+    let savedPosts = data.savedPosts;
+    savedPosts.push(id);
+
+    if (save.src == "http://127.0.0.1:5500/img/save.png") {
+      save.src = "http://127.0.0.1:5500/img/saved.png";
+  
+      fetch("URL" + userId, {
+        method: "PUT",
+        headers: {
+            "content-type": "application/json",
+            credentials: "same-origin",
+        },
+        body: JSON.stringify({
+            savedPosts: savedPosts
+        })
+      })
+  
+    } else {
+      save.src = "http://127.0.0.1:5500/img/save.png";
+
+      let postToUnsave = savedPosts.indexOf(id);
+
+      if(postToUnsave > -1) {
+        savedPosts.splice(postToUnsave, 1);
+      }
+
+      fetch("URL" + userId, {
+        method: "PUT",
+        headers: {
+            "content-type": "application/json",
+            credentials: "same-origin",
+        },
+        body: JSON.stringify({
+            savedPosts: savedPosts
+        })
+      })
+    }
+  })
+}
+
