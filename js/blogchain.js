3a65953f16e65b0239fc05aeff7e5e9c9c6c75ca
Lukas Frey <l.frey.inf21@stud.bbbaden.ch>
2024-02-20 13:18:35 +0100

    Updated like and save function

--- js/blogchain.js
+++ js/blogchain.js
@@ -3,6 +3,7 @@ import { likePost, savePost } from "./postInteractions.js"
 var jwt = sessionStorage.getItem("token");
 const postContainer = document.getElementById("postContainer");
 var accountId = "";
+var accountIdSavedPosts = [];
 
 fetch("https://blogchainapi.onrender.com/api/User/Get-Me", {
   headers: {
@@ -13,7 +14,9 @@ fetch("https://blogchainapi.onrender.com/api/User/Get-Me", {
 })
 .then((response) => response.json())
 .then(data => {
-  accountId = data.id;
+    accountId = data.id;
+    accountIdSavedPosts = data.savedPosts;
+    console.log(accountIdSavedPosts)
 })
 
 fetch("https://blogchainapi.onrender.com/api/Post/GetAll", {
@@ -26,65 +29,85 @@ fetch("https://blogchainapi.onrender.com/api/Post/GetAll", {
 .then((json) => {
     json.forEach(element => {
 
-        
+        fetch(`https://blogchainapi.onrender.com/api/User/GetProfile?id=${element.authorId}`, {
+            headers: {
+                "content-type": "application/json",
+                credentials: "same-origin",
+            }
+        })
+        .then((response) => response.json())
+        .then(authorData => {
 
-        let post = document.createElement("div");
-        post.classList.add("post");
-        post.id = element.id;
-        
-        let topline = document.createElement("div");
-        topline.classList.add("topLine");
-        let profileImg = document.createElement("img");
-        profileImg.src = "element.image";
-        profileImg.alt = "";
-        let username = document.createElement("span");
-        username.classList.add("username");
-        username.innerHTML = element.username;
-        let time = document.createElement("span");
-        time.classList.add("time");
-        time.innerHTML = element.published;
+            let post = document.createElement("div");
+            post.classList.add("post");
+            post.id = element.id;
+            
+            let topline = document.createElement("div");
+            topline.classList.add("topLine");
+            let profileImg = document.createElement("img");
+            profileImg.src = authorData.username;
+            profileImg.alt = "";
+            let username = document.createElement("span");
+            username.classList.add("username");
+            username.innerHTML = authorData.profileImage;
+            let time = document.createElement("span");
+            time.classList.add("time");
+            time.innerHTML = element.published;
+    
+            topline.appendChild(profileImg);
+            topline.appendChild(username);
+            topline.appendChild(time);
+            post.appendChild(topline);
+    
+            let content = document.createElement("div");
+            content.classList.add("content");
+            let caption = document.createElement("div");
+            caption.classList.add("caption");
+            caption.innerHTML = element.text;
+    
+            content.appendChild(caption);
+            post.appendChild(content);
+    
+            let bottomline = document.createElement("div");
+            bottomline.classList.add("bottomLine");
 
-        topline.appendChild(profileImg);
-        topline.appendChild(username);
-        topline.appendChild(time);
-        post.appendChild(topline);
+            let likebtn = document.createElement("img");
+            if(element.likedBy.includes(accountId)) {
+                likebtn.src = "http://127.0.0.1:5500/img/likedNew.png";
+            } else{
+                likebtn.src = "http://127.0.0.1:5500/img/like.png";
+            }
+            likebtn.alt = "";
+            likebtn.id = `like${element.id}`;
 
-        let content = document.createElement("div");
-        content.classList.add("content");
-        let caption = document.createElement("div");
-        caption.classList.add("caption");
-        caption.innerHTML = element.text;
+            let commentbtn = document.createElement("img");
+            commentbtn.src = "http://127.0.0.1:5500/img/comment.png";
+            commentbtn.alt = "";
 
-        content.appendChild(caption);
-        post.appendChild(content);
-
-        let bottomline = document.createElement("div");
-        bottomline.classList.add("bottomLine");
-        let likebtn = document.createElement("img");
-        likebtn.src = "http://127.0.0.1:5500/img/like.png";
-        likebtn.alt = "";
-        likebtn.id = `like${element.id}`;
-        let commentbtn = document.createElement("img");
-        commentbtn.src = "http://127.0.0.1:5500/img/comment.png";
-        commentbtn.alt = "";
-        let savebtn = document.createElement("img");
-        savebtn.src = "http://127.0.0.1:5500/img/save.png";
-        savebtn.alt = "";
-        savebtn.id = `save${element.id}`;
-
-        bottomline.appendChild(likebtn);
-        bottomline.appendChild(commentbtn);
-        bottomline.appendChild(savebtn);
-        post.appendChild(bottomline);
-
-        postContainer.appendChild(post);
-
-        document.getElementById(`like${element.id}`).onclick = function() {
-            likePost(element.id);
-        };
-
-        document.getElementById(`save${element.id}`).onclick = function() {
-            savePost(element.id);
-        };
+            console.log(accountIdSavedPosts[0])
+            let savebtn = document.createElement("img");
+            if(accountIdSavedPosts[0].includes(element.id)) {
+                savebtn.src = "http://127.0.0.1:5500/img/saved.png";
+            } else {
+                savebtn.src = "http://127.0.0.1:5500/img/save.png";
+            }
+            savebtn.alt = "";
+            savebtn.id = `save${element.id}`;
+    
+            bottomline.appendChild(likebtn);
+            bottomline.appendChild(commentbtn);
+            bottomline.appendChild(savebtn);
+            post.appendChild(bottomline);
+    
+            postContainer.appendChild(post);
+    
+            document.getElementById(`like${element.id}`).onclick = function() {
+                likePost(element.id);
+            };
+    
+            document.getElementById(`save${element.id}`).onclick = function() {
+                savePost(element.id);
+            };        
+        })
     });
 })
